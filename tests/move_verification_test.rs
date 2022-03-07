d5593227d0254349a9ae99db1ef9acf64b87c5d9
Zekun Li <li.zekun@gmail.com>
2022-03-07 14:48:48 -0800

    rename dir diem-framework to framework
    GitOrigin-RevId: 227e208d33302c0d1f5b07f2445077986bb2f1fb

---
+++ tests/move_verification_test.rs
@@ -0,0 +1,13 @@
+// Copyright (c) The Aptos Foundation
+// SPDX-License-Identifier: Apache-2.0
+
+// use move_cli::package::prover::ProverTest;
+
+// TODO: split this into individual tests once the package system supports this.
+#[test]
+fn prove_all() {
+    // All prover tests are temporarily disabled
+    //    ProverTest::create("core").run();
+    //    ProverTest::create("experimental").run();
+    //    ProverTest::create("DPN").run()
+}
